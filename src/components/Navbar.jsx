f7a2c87ee511da72cbb6ac56cc531a46e0ea1c7d
EllisDann <ellisdann@icloud.com>
2026-02-05 14:28:40 +0000

    Started Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import "./Navbar.css";
+
+
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        
+        <div className="logo">MS Traffic</div>
+
+        <div className="nav-links">
+          <a href="#whatwedo">What We Do</a>
+          <a href="#audit">Road Safety Audit</a>
+          <a href="#about">About</a>
+          <a href="#contact">Contact</a>
+        </div>
+
+      </div>
+    </nav>
+  )
+}
